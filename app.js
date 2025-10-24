a7cb7ea7e6b52a97d96a827ca684cc8bac6e2a01
isugoel.21 <ishu.22scse1180016@galgotiasuniversity.edu.in>
2025-10-25 05:22:39 +0530

    main: rename to calculateTotal v2

--- app.js
+++ app.js
@@ -1,13 +1,1 @@
-
-
-function computeFinalTotal(items) {
-  return items.reduce((s, i) => s + i, 0);
-}
-
-
-function computeFinalTotal(items) {
-  return items.reduce((s, i) => s + i, 0);
-}
-
-
-feature / rename;
+function calculateTotal(items) {\n  // main implementation v2\n  return items.reduce((s,i)=>s+i,0);\n}\n
